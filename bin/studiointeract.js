18e76f1e4dfd3d0c9e09b53111e86228a73ad3c2
Tim Brandin <tim.brandin@gmail.com>
2015-07-22 16:25:10 +0200

    Hey, I'm coming somewhere.

--- bin/studiointeract.js
+++ bin/studiointeract.js
@@ -1,4 +1,35 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
 
-shell.exec("echo shell.exec works");
+var path = require('path');
+var cjson = require('cjson');
+var Config = require('../lib/config');
+var ActionsRegistry = require('../lib/actions');
+var helpers = require('../lib/helpers');
+require('colors');
+
+console.log('\nStudio Interact: Drupal Deployments'.bold.blue);
+console.log('-----------------------------------\n'.bold.blue);
+
+var action = process.argv[2];
+if(action == 'init') {
+  //special setup for init
+  ActionsRegistry.init();
+} else {
+  var cwd = path.resolve('.');
+  //read config and validate it
+  var config = Config.read();
+  runActions(config, cwd);
+}
+
+function runActions(config, cwd) {
+  var actionsRegistry = new ActionsRegistry(config, cwd);
+  if(actionsRegistry[action]) {
+    actionsRegistry[action]();
+  } else {
+    if(typeof action !== "undefined") {
+      var errorMessage = 'No Such Action Exists: ' + action;
+      console.error(errorMessage.bold.red);
+    }
+    helpers.printHelp();
+  }
+}
---
+++ example/settings.php
@@ -0,0 +1,3 @@
+<?php
+
+// Drupal settings goes here.
