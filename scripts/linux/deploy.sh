07c9f9db33ff890b06f909817831a08a8acbf28a
Tim Brandin <tim.brandin@gmail.com>
2015-09-01 14:58:17 +0200

    Added friendly try with sudo if available.

--- scripts/linux/deploy.sh
+++ scripts/linux/deploy.sh
@@ -7,6 +7,13 @@ if ! grep -q $GITHOST ~/.ssh/config; then
   echo -e "Host $GITHOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 fi
 
+# Use sudo if available.
+SUDO=""
+CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1 | grep "load" | wc -l)
+if [ ${CAN_I_RUN_SUDO} -gt 0 ]; then
+  SUDO="sudo"
+fi;
+
 # DEPLOY
 
 # Clone repository if first deploy.
@@ -14,12 +21,12 @@ if [ ! -d <%= installLocation %>/current ]; then
   cd <%= installLocation %>
   git clone -b master <%= repository %> current
 else
-  rm -rf <%= installLocation %>/temp
+  $SUDO rm -rf <%= installLocation %>/temp
 
   # Copy source files.
-  if [ ! -d <%= installLocation %>/previous ]; then
+  if [ -d <%= installLocation %>/previous ]; then
     mv <%= installLocation %>/previous <%= installLocation %>/temp
-    rsync -az <%= installLocation %>/current <%= installLocation %>/temp
+    rsync -az <%= installLocation %>/current/ <%= installLocation %>/temp/
   else
     cp -R <%= installLocation %>/current <%= installLocation %>/temp
   fi
@@ -31,31 +38,29 @@ else
   cd <%= installLocation %>
 
   # Switch versions, current => previous && temp => current.
-  rm -rf <%= installLocation %>/previous
+  $SUDO rm -rf <%= installLocation %>/previous
   mv <%= installLocation %>/current <%= installLocation %>/previous
   mv <%= installLocation %>/temp <%= installLocation %>/current
 fi
 
 # Set default permissions.
-find <%= installLocation %>/current/<%= web %> -type f -exec chmod 644 {} +
-find <%= installLocation %>/current/<%= web %> -type d -exec chmod 755 {} +
-#chmod -R 754 <%= installLocation %>/current/<%= web %>
-#chmod -R 760 <%= installLocation %>/current/<%= web %>/index.php
+find <%= installLocation %>/current/<%= web %> -type f -exec $SUDO chmod 644 {} +
+find <%= installLocation %>/current/<%= web %> -type d -exec $SUDO chmod 755 {} +
 
-find <%= installLocation %>/default -type f -exec chmod 644 {} +
-find <%= installLocation %>/default -type d -exec chmod 755 {} +
-# chmod -R 754 <%= installLocation %>/default
-# chmod -R 775 <%= installLocation %>/default/files
-chmod 744 <%= installLocation %>/default/settings.php
+find <%= installLocation %>/default -type f -exec $SUDO chmod 644 {} +
+find <%= installLocation %>/default -type d -exec $SUDO chmod 755 {} +
+$SUDO chmod 744 <%= installLocation %>/default/settings.php
 
 
-# Set group.
-chgrp -R <%= group %> <%= installLocation %>/current/<%= web %>
-chgrp -R <%= group %> <%= installLocation %>/default
+# Set group and owner.
+$SUDO chown -R <%= group %>:<%= group %> <%= installLocation %>/current/<%= web %>
+$SUDO chown -R <%= group %>:<%= group %> <%= installLocation %>/default
 
 # Remove default settings.
-rm -rf <%= installLocation %>/current/<%= web %>/sites/default
+$SUDO rm -rf <%= installLocation %>/current/<%= web %>/sites/default
 
 # Create symbolic link to settings and files.
-rm <%= installLocation %>/current/<%= web %>/sites/default
-ln -sf <%= installLocation %>/default <%= installLocation %>/current/<%= web %>/sites/default
+if [ -d <%= installLocation %>/current/<%= web %>/sites/default ]; then
+  $SUDO rm <%= installLocation %>/current/<%= web %>/sites/default
+fi
+$SUDO ln -sf <%= installLocation %>/default <%= installLocation %>/current/<%= web %>/sites/default
